9c6a63eef8e68f478cba4ad233759937e10483c4
Curt Miller <curtdmiller@gmail.com>
2020-01-23 22:08:53 -0500

    update title and header text

--- src/Components/layout/header.js
+++ src/Components/layout/header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return (
         <header>
             <h1>Herman Miller Brochures</h1>
-            <p>SVG versions of Herman Miller trifold brochures from 1960 designed by George Nelson (+ Irving Harper, Don Ervin, Tony Zamora, Dick Schiffer).</p>
+            <p>SVG versions of Herman Miller trifold brochures from 1960 designed by George Nelson's studio (Irving Harper, Don Ervin, Tony Zamora, Dick Schiffer).</p>
             <p>See also:</p>
             <p><a href="http://projectobjectshop.com/hermanmiller-trifold#/id/i12535826">Set of 12 at Project Object</a>, <a href="http://www.javiergd.com/blog/?p=2341">Javier Garcia ephemera blog post</a></p>
         </header>
